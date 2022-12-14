a4bbe21881f2140b06b0c107a85dc4f3ff557dac
Kylah Arnold <Karnold0144@gmail.com>
2022-12-14 09:23:19 -0500

    trade services

---
+++ capstone/finalVue/vue2/vue/src/views/Trade.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+       <Header></Header>
+  <div class="Main">
+  </div>
+</div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+export default {
+  name: "games",
+  components:{
+    Header
+  }
+}
+</script>
